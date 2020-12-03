20480101220a0100bcb96230cbc94dac22475e34
Reed Roffis <reedroffis18@gmail.com>
2020-12-03 16:31:48 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -118,13 +118,13 @@ def game_hash
       :slam_dunks => 0
       },
       {
-      :player_name => "Brendan Haywood",
+      :player_name => "Kemba Walker",
       :number => 33,
       :shoe => 15,
       :points => 6,
       :rebounds => 12,
       :assists => 12,
-      :steals => 22,
+      :steals => 7,
       :blocks => 5,
       :slam_dunks => 12
       }
